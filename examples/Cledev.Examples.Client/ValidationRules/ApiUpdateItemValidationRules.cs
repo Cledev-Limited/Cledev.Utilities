18de1facee15e287ab5738dda926428712d1a057
lucabriguglia <luca.briguglia@outlook.com>
2023-04-15 19:07:27 +0100

    WIP

---
+++ examples/Cledev.Examples.Client/ValidationRules/ApiUpdateItemValidationRules.cs
@@ -0,0 +1,19 @@
+﻿using Cledev.Client.Services;
+using Cledev.Examples.Shared;
+
+namespace Cledev.Examples.Client.ValidationRules;
+
+public class ApiUpdateItemValidationRules : IUpdateItemValidationRules
+{
+    private readonly ApiService _apiService;
+
+    public ApiUpdateItemValidationRules(ApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
+    public async Task<bool> IsItemNameUnique(Guid id, string name)
+    {
+        return await _apiService.GetFromJsonAsync<bool>($"api/items/is-name-unique/{name}?id={id}");
+    }
+}
---
+++ examples/Cledev.Examples.Server/Handlers/CacheHandlers.cs
@@ -0,0 +1,35 @@
+﻿using Cledev.Core.Events;
+using Cledev.Core.Results;
+using Cledev.Examples.Shared;
+using Cledev.Server.Caching;
+
+namespace Cledev.Examples.Server.Handlers;
+
+public class CacheHandlers : 
+    IEventHandler<ItemCreated>,
+    IEventHandler<ItemDeleted>,
+    IEventHandler<ItemUpdated>
+{
+    private readonly ICacheManager _cacheManager;
+
+    public CacheHandlers(ICacheManager cacheManager)
+    {
+        _cacheManager = cacheManager;
+    }
+
+    public async Task<Result> Handle(ItemCreated @event, CancellationToken cancellationToken) => 
+        await ClearItemsCache();
+
+    public async Task<Result> Handle(ItemDeleted @event, CancellationToken cancellationToken) => 
+        await ClearItemsCache();
+
+    public async Task<Result> Handle(ItemUpdated @event, CancellationToken cancellationToken) => 
+        await ClearItemsCache();
+
+    private async Task<Result> ClearItemsCache()
+    {
+        await Task.CompletedTask;
+        _cacheManager.Remove("Items");
+        return Result.Ok();
+    }
+}
