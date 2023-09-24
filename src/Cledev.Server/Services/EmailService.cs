d37be3d8c2b6310b6c3f8de0109bfd64169cc42b
lucabriguglia <luca.briguglia@outlook.com>
2023-09-24 12:19:06 +0100

    Update email service and other small changes

--- src/Cledev.Server/Services/EmailService.cs
+++ src/Cledev.Server/Services/EmailService.cs
@@ -1,34 +1,65 @@
 using System.Net;
 using System.Net.Mail;
-using Cledev.Server.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Cledev.Server.Services;
 
+public class EMailSettings
+{
+    public string Address { get; set; } = null!;
+    public string DisplayName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+}
+
 public interface IEmailService
 {
-    Task SendEmail(string address, string subject, string htmlMessage);
+    Task SendEmail(string subject, string body, string toAddress);
+    
+    Task SendEmail(
+        string subject, 
+        string body, 
+        IEnumerable<string> toAddresses, 
+        IEnumerable<string>? ccAddresses = null, 
+        IEnumerable<string>? bccAddresses = null,
+        MailPriority? priority = null,
+        IEnumerable<Attachment>? attachments = null,
+        IDictionary<string, string>? emailHeaders = null);
 }
 
 public class EmailService : IEmailService
 {
-    private readonly MailSettings _settings;
+    private readonly EMailSettings _settings;
     private readonly ILogger<EmailService> _logger;
 
     [ActivatorUtilitiesConstructor]
-    public EmailService(IOptions<MailSettings> settings, ILogger<EmailService> logger) : this(settings.Value, logger)
+    public EmailService(IOptions<EMailSettings> settings, ILogger<EmailService> logger) : this(settings.Value, logger)
     {
     }
     
-    public EmailService(MailSettings settings, ILogger<EmailService> logger)
+    public EmailService(EMailSettings settings, ILogger<EmailService> logger)
     {
         _settings = settings;
         _logger = logger;
     }
 
-    public async Task SendEmail(string address, string subject, string htmlMessage)
+    public async Task SendEmail(string subject, string body, string toAddress)
+    {
+        await SendEmail(subject, body, new List<string> { toAddress });
+    }
+    
+    public async Task SendEmail(
+        string subject, 
+        string body, 
+        IEnumerable<string> toAddresses, 
+        IEnumerable<string>? ccAddresses = null, 
+        IEnumerable<string>? bccAddresses = null,
+        MailPriority? priority = null,
+        IEnumerable<Attachment>? attachments = null,
+        IDictionary<string, string>? emailHeaders = null)
     {
         try
         {
@@ -36,13 +67,18 @@ public class EmailService : IEmailService
             {
                 From = new MailAddress(_settings.Address, _settings.DisplayName),
                 Subject = subject,
+                Body = body,
                 IsBodyHtml = true,
-                Body = htmlMessage
+                Priority = priority ?? MailPriority.Normal
             };
 
-            message.To.Add(new MailAddress(address));
+            toAddresses.ToList().ForEach(address => message.To.Add(new MailAddress(address)));
+            ccAddresses?.ToList().ForEach(address => message.CC.Add(new MailAddress(address)));
+            bccAddresses?.ToList().ForEach(address => message.Bcc.Add(new MailAddress(address)));
+            emailHeaders?.ToList().ForEach(header => message.Headers.Add(header.Key, header.Value));
+            attachments?.ToList().ForEach(attachment => message.Attachments.Add(attachment));
 
-            var smtp = new SmtpClient
+            var client = new SmtpClient
             {
                 Port = _settings.Port,
                 Host = _settings.Host,
@@ -52,7 +88,7 @@ public class EmailService : IEmailService
                 DeliveryMethod = SmtpDeliveryMethod.Network
             };
 
-            await smtp.SendMailAsync(message);
+            await client.SendMailAsync(message);
         }
         catch (Exception ex)
         {
